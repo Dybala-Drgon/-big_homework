200d50d031d6a4ec3e319a9b76569f9a61374b6c
Dybala-Drgon <59010060+Dybala-Drgon@users.noreply.github.com>
2020-07-16 21:24:48 +0800

    下一步模块化测试

---
+++ 通讯录0.1.2/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include"User.h"
+#include"Interface.h"
+#include"Manager.h"
+using namespace std;
+extern User* gl_User_Head;
+extern Manager man;
+
+int main() {
+	Interface test;
+	test.Display();
+	//User t;
+	//t.Display();
+	return 0;
+}
