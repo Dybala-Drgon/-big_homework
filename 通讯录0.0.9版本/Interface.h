aad1f2e3a7dd2eeb24ec16fd38a9fefbcfb317d0
Dybala-Drgon <59010060+Dybala-Drgon@users.noreply.github.com>
2020-07-16 14:30:58 +0800

    Add files via upload

---
+++ 通讯录0.0.9版本/Interface.h
@@ -0,0 +1,20 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+#include<iostream>
+#include<string>
+#include"User.h"
+#include"Manager.h"
+using namespace std;
+
+//主要是处理登录界面，提供登录或者注册接口;
+class Interface {
+public:
+	void Menu();
+	void Display();
+	User* Log_In_User();//登录函数
+	bool Log_In_Manager();
+	bool Register_User();//注册
+	User* Search_User_ID(IN_PARAM string first);
+
+};
+#endif
